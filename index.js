0277cd28ff31150adcd04de64788f39491fcfa78
Ayush <badsoulak@gmail.com>
2024-02-02 02:52:54 +0530

    commiting

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", this.window.scrollY > 100);
 });
 
+
+
 let menu = document.querySelector("#menu-icon");
 let nav = document.querySelector(".navlist");
 
@@ -16,6 +18,8 @@ window.onscroll = () => {
   menu.classList.remove("bx-x");
   nav.classList.remove("open");
 };
+
+
 const sr = ScrollReveal({
   distance: "25px",
   duration: 2500,
